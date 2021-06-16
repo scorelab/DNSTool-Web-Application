7e33a697236239ddfdb661c8a6370f10d814a064
Sahan <shnrndk@gmail.com>
2021-06-17 01:21:16 +0530

    make dashboard responsive

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -5,12 +5,18 @@ import Button from '@material-ui/core/Button';
 import NewsCard from '../newsCard';
 import ScanTable from '../scanTable';
 import Scanbuttons from './Scanbuttons';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { useTheme } from '@material-ui/core/styles';
 
 function Dashboard() {
+
+    const theme = useTheme();
+    const isMobile = useMediaQuery(theme.breakpoints.down('md'));
+
     return (
         <>
             <Navbar />
-            <Grid container spacing={2} style={{ padding: '10px 30px 0px 70px' }}>
+            <Grid container spacing={2} style={{ padding: !isMobile ? '10px 30px 0px 70px' : '10px 20px' }}>
                 <Grid item xs={12} md={9} >
                     <div style={{ display: 'flex', flexDirection: 'column' }}>
                         <Scanbuttons />
