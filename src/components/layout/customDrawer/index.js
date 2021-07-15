dd9952d42f67551e0fbe027ff1661694fa604b83
Sahan <shnrndk@gmail.com>
2021-07-15 17:35:08 +0530

    added sidebar

---
+++ src/components/layout/customDrawer/index.js
@@ -0,0 +1,63 @@
+import React, { useEffect } from 'react'
+import Box from '@material-ui/core/Box';
+import Divider from '@material-ui/core/Divider';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import MailIcon from '@material-ui/icons/Mail';
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
+
+const drawerWidth = 240;
+
+
+function CustomDrawer({ isOpen, open, close }) {
+
+
+
+    const list = (anchor) => (
+        <Box
+            sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
+            role="presentation"
+        >
+            <List>
+                {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                    <ListItem button key={text}>
+                        <ListItemIcon>
+                            {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                        </ListItemIcon>
+                        <ListItemText primary={text} />
+                    </ListItem>
+                ))}
+            </List>
+            <Divider />
+            <List>
+                {['All mail', 'Trash', 'Spam'].map((text, index) => (
+                    <ListItem button key={text}>
+                        <ListItemIcon>
+                            {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                        </ListItemIcon>
+                        <ListItemText primary={text} />
+                    </ListItem>
+                ))}
+            </List>
+        </Box>
+    );
+
+
+    return (
+        <>
+            <SwipeableDrawer
+                anchor='left'
+                open={isOpen}
+                onClose={close}
+                onOpen={open}
+            >
+                {list('left')}
+            </SwipeableDrawer>
+        </>
+    )
+}
+
+export default CustomDrawer
--- src/components/layout/navbar/index.js
+++ src/components/layout/navbar/index.js
@@ -7,6 +7,7 @@ import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import { makeStyles } from '@material-ui/styles';
 import CreateScanModal from '../../createScan';
+import CustomDrawer from '../customDrawer';
 
 
 function Navbar() {
@@ -17,15 +18,29 @@ function Navbar() {
         }
     });
 
+    const [state, setstate] = useState({
+        left: false
+    })
+
     const classes = useStyles();
 
+    const openDrawer = () => {
+        setstate({
+            left: true
+        })
+    }
 
+    const closeDrawer = () => {
+        setstate({
+            left: false
+        })
+    }
 
     return (
         <>
-            <AppBar position="static" sx={{ display: 'flex',height:'45px',justifyContent:'center' }} elevation={0} >
+            <AppBar position="static" sx={{ display: 'flex', height: '45px', justifyContent: 'center', zIndex: 150, zIndex: (theme) => theme.zIndex.drawer + 1 }} elevation={0} >
                 <Toolbar>
-                    <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+                    <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }} onClick={openDrawer}>
                         <MenuIcon />
                     </IconButton>
                     <Typography variant="h6" component="div" sx={{ flexGrow: 1, textAlign: 'left' }}>
@@ -33,6 +48,7 @@ function Navbar() {
                     </Typography>
                     {/* <Button onClick={handleOpen} className={classes.button} variant="outlined" >Create Scan</Button>
                     <CreateScanModal open={openCreateScanModal} handleClose={handleClose} /> */}
+                    <CustomDrawer isOpen={state.left} open={openDrawer} close={closeDrawer} />
                 </Toolbar>
             </AppBar>
         </>
